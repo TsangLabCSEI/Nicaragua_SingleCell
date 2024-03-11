a62b81913cb2605892606ba9b2f44992e0778309
Leon Bichmann <leon@Leons-Air.med.yale.internal>
2024-03-11 11:23:09 -0400

    fixes in scripts

--- incr_decr_variance/scripts/subset_data_ctrl_cells.R
+++ incr_decr_variance/scripts/subset_data_ctrl_cells.R
@@ -2,19 +2,18 @@ library(SummarizedExperiment)
 library(limma)
 
 
-CELLTYPE <- snakemake@input[["celltype"]]
 
 #change input file based on celltype
-vst_IN_PATH <- paste0("data/processed/NICA_batch1to4_new_normalized_",CELLTYPE,".rds")
+vst_IN_PATH <- snakemake@input[[1]] #paste0("data/processed/NICA_batch1to4_new_normalized_",CELLTYPE,".rds")
 
 #change OUT paths
-OUT_DIR <- paste0("data/analysis_out/variancePartition/sliding_age_window/subsetted_vst_ctrl_cells_",CELLTYPE)
+OUT_DIR <- dirname(snakemake@output[[1]]) #paste0("data/analysis_out/variancePartition/sliding_age_window/subsetted_vst_ctrl_cells_",CELLTYPE)
 dir.create(OUT_DIR, recursive = TRUE)
 
 vst <- readRDS(vst_IN_PATH)
 
 #vst <- vst[, vst$Sample.type == "case"]
-meta <- as.data.frame(colData(vst))
+meta <- as.data.frame(vst$samples)
 
 min_age_mo <- min(meta$matched.timepoint.age)
 max_age_mo <- max(meta$matched.timepoint.age)
@@ -36,7 +35,7 @@ vst_window_list <- lapply(1:nrow(window_dat), function(i){
 })
 
 lapply(vst_window_list, dim)
-lapply(vst_window_list, function(x){table(table(x$matched.individual))})
+lapply(vst_window_list, function(x){table(table(x$samples$matched.individual))})
 
 for(i in seq_along(vst_window_list)){
   window_name <- window_dat$window_name[[i]]
