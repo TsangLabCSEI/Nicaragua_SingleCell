93e325397993ac9f04264bc02d2a2ec655caca55
Leon Bichmann <leon@MWPF486AHX.yu.yale.edu>
2025-01-13 11:34:51 -0500

    add bulk subj var and heatmap function

--- ImmuneAgeStability/R/Main.R
+++ ImmuneAgeStability/R/Main.R
@@ -13,7 +13,6 @@ library(scales)
 #'
 #' @param signature vector of gene symbols
 #' @return A data frame of subject variance per gene and celltype
-#' @export
 compute_signature_stability<-function(signature){
   subj_vars<-list()
   for (ct in unique(gene_stability_sc_avg$celltype)){
@@ -39,11 +38,35 @@ compute_signature_stability<-function(signature){
 #' @param signature_name string of signature name
 #' @param signature_stability_df data frame of subject variance per gene and celltype
 #' @return violinplot
-#' @export
 plot_signature_stability<-function(signature_name,signature_stability_df){
   vioplot(subject_variance~celltype, data=signature_stability_df[signature_stability_df$subset=="foreground",], col = "orange", plotCentre = "line", side = "left", horizontal=FALSE, las=2, xlab="", ylab=paste0(signature_name," subject variance"))
   vioplot(subject_variance~celltype, data=signature_stability_df[signature_stability_df$subset=="background",], col = "lightblue", plotCentre = "line", side = "right", horizontal=FALSE, las=2, add=TRUE, ylab=paste0(signature_name," subject variance"))
   legend("topright", legend=c(signature_name, "background"),fill=c("orange", "lightblue"), cex = 0.6)
+}
+
+
+#' Heatmap of Subject Variance per Signature
+#'
+#' This function takes a vector of signature names as input and 
+#' visualizes the distribution of its subject variance in contrast 
+#' to the background in a violinplot.
+#'
+#' @param signature_names vector of signature names
+#' @return heatmap
+plot_signature_stability_heatmap<-function(signature_names){
+  stability_df_list<-list()
+  for (sig in signature_names){
+        df<-compute_signature_stability(baseline_signatures[[sig]])
+        df$signature<-sig
+        stability_df_list[[sig]]<-df
+  }
+  stability_df<-do.call(rbind,stability_df_list)
+  stability_df<-stability_df[stability_df$subset=="foreground",]
+  sig_subj_vars_all_fg<-as.data.frame(stability_df %>% group_by(celltype,signature) %>% summarize(median(subject_variance)))
+  colnames(sig_subj_vars_all_fg)<-c("celltype","signature","subject_variance")
+  celltypes<-c("bulk","B_Mem","B_Naive","CD4_Mem","CD4_Naive","CD8_Mem","CD8_Naive","ILC","MAIT","gdT_Vd1","gdT_Vd2","Mono_Classical","Mono_NonClassical","NK_CD16hi","NK_CD56hi","cDC","pDC")
+  sig_subj_vars_all_fg$celltype<-factor(sig_subj_vars_all_fg$celltype, levels=celltypes)
+  ggplot(data=sig_subj_vars_all_fg[sig_subj_vars_all_fg$celltype!="ILC",],aes(x=signature,y=celltype,fill=subject_variance))+geom_tile()  + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) + scale_fill_viridis_c(option = "magma")
 }
 
 
@@ -58,7 +81,6 @@ plot_signature_stability<-function(signature_name,signature_stability_df){
 #' @param signature_name string of signature name
 #' @param signature vector of gene symbols
 #' @return data frame of subject variance rate of change per gene and celltype
-#' @export
 compute_signature_stability_rateofchange<-function(signature_name,signature){
   svar_sig_df_list_ct<-list()
   for (ct in names(gene_stability)){
@@ -86,7 +108,6 @@ compute_signature_stability_rateofchange<-function(signature_name,signature){
 #' @param x string of gene name
 #' @param df data frame of subject variance rate of change
 #' @return linear model coefficients
-#' @export
 calclmodp<-function(x,df){
   form <- mean_vexp ~ window_number
   lmod<-lm(form,df[df$gene==x,])
@@ -109,7 +130,6 @@ calclmodp<-function(x,df){
 #' @param signature_name string of signature name
 #' @param signature_gene_list vector of gene symbols
 #' @return data frame of subject variance slope of rate of change
-#' @export
 compute_signature_stability_slope<-function(signature_name,signature_gene_list){
   svar_sig_rate_df<-compute_signature_stability_rateofchange(signature_name,signature_gene_list)
   svar_sig_slope_list<-list()
@@ -136,7 +156,6 @@ compute_signature_stability_slope<-function(signature_name,signature_gene_list){
 #'
 #' @param signature_stability_slope data frame of subject variance slope of rate of change
 #' @return radarplot
-#' @export
 plot_signature_stability_radar<-function(signature_stability_slope){
   incr_decr_list<-list()
   for (ct in unique(signature_stability_slope$celltype)){
@@ -153,7 +172,7 @@ plot_signature_stability_radar<-function(signature_stability_slope){
   data <- rbind(rep(1,dim(df_incr_decr)[2]) , rep(0,dim(df_incr_decr)[2]) , df_incr_decr)
 
   radarchart(data,pcol = hue_pal()(3))
-  legend(x=1.1, y=1.3, legend = rownames(data[-c(1,2),]), bty = "n", pch=20 , cex=0.8, pt.cex=1.5,col=hue_pal()(3))
+  legend(x=1.1, y=1.35, legend = rownames(data[-c(1,2),]), bty = "n", pch=20 , cex=0.8, pt.cex=1.5,col=hue_pal()(3))
   title(sig)
 }
 
@@ -167,7 +186,6 @@ plot_signature_stability_radar<-function(signature_stability_slope){
 #' @param signature_rateOfchange data frame of subject variance rate of change for gene signature
 #' @param cell_subsets celltypes to be visualized
 #' @return lineplot
-#' @export
 plot_signature_agetrend<-function(signature_rateOfchange, cell_subsets){
   sig<-unique(signature_rateOfchange$signature)
   ggplot(data=signature_rateOfchange[signature_rateOfchange$celltype %in% cell_subsets,], aes(x=window_number,y=mean_vexp, group=celltype, colour=celltype))+ geom_smooth(method = "loess") + theme_bw()+ggtitle(paste0(sig," - ",paste(cell_subsets,collapse = " / ")))
@@ -183,7 +201,6 @@ plot_signature_agetrend<-function(signature_rateOfchange, cell_subsets){
 #' @param signature_rateOfchange data frame of subject variance rate of change for gene signature
 #' @param cell_subsets celltypes to be visualized
 #' @return lineplot
-#' @export
 plot_signature_gene_agetrend<-function(signature_rateOfchange, cell_subsets){
   sig<-unique(signature_rateOfchange$signature)
   ggplot(data=signature_rateOfchange[signature_rateOfchange$celltype %in% cell_subsets,], aes(x=window_number,y=mean_vexp, group=gene, colour=gene))+ geom_smooth(method = "loess", se = F) + theme_bw()+ggtitle(paste0(sig," - ",paste(cell_subsets,collapse = " / ")))
