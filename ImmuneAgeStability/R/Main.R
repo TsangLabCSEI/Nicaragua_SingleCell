af77a8c67881b38d5c98c57d7c2a72b19c51b88b
Leon Bichmann <lb2336@login2.mccleary.ycrc.yale.edu>
2025-02-25 17:26:26 -0500

    add multi-signature stability and stacked barplots, change heatmap

--- ImmuneAgeStability/R/Main.R
+++ ImmuneAgeStability/R/Main.R
@@ -29,6 +29,38 @@ compute_signature_stability<-function(signature){
 }
 
 
+#' Compute Multi-Signature Stability
+#'
+#' This function computes the average single cell subject variance for a gene signature.
+#' Input is a vector of gene symbols and it retrieves the distribution of its subject
+#' variance to the distribution of all genes within each celltype as background.
+#'
+#' @param signatures named list of vectors of gene symbols
+#' @return A data frame of subject variance per signature and celltype
+compute_multisignature_stability<-function(signatures){
+  gene_stability_all<-rbind(bulk_subj_var_gene_df,gene_stability_sc_avg)
+  sig_subj_vars<-list()
+  for (sig in names(signatures)){
+    subj_vars<-list()
+    for (ct in unique(gene_stability_all$celltype)){
+      df_svar<-gene_stability_all[gene_stability_all$celltype==ct,]
+      df_svar<-df_svar[df_svar$gene %in% signatures[[sig]],]
+      df_svar<-df_svar[!is.na(df_svar$batch),]
+      subj_vars[[ct]]<-cbind(df_svar$subject_variance_explained,ct)
+    }
+    df_svar_ct<-as.data.frame(do.call(rbind,subj_vars))
+    colnames(df_svar_ct)<-c("subject_variance","celltype")
+    df_svar_ct$subject_variance<-as.numeric(df_svar_ct$subject_variance)
+    df_svar_ct<-as.data.frame(df_svar_ct %>% group_by(celltype) %>% summarize(avg_subject_variance=mean(subject_variance)))
+    df_svar_ct$signature<-sig
+    sig_subj_vars[[sig]]<-df_svar_ct
+  }
+  df_svar_ct_sigs<-as.data.frame(do.call(rbind,sig_subj_vars))
+  df_svar_ct_sigs$subject_variance<-as.numeric(df_svar_ct_sigs$avg_subject_variance)
+  return(df_svar_ct_sigs)
+}
+
+
 #' Plot Violins of Subject Variance
 #'
 #' This function takes a signature name and single cell subject variance data frame as
@@ -51,22 +83,12 @@ plot_signature_stability<-function(signature_name,signature_stability_df){
 #' visualizes the distribution of its subject variance in contrast 
 #' to the background in a violinplot.
 #'
-#' @param signature_names vector of signature names
+#' @param A data frame of subject variance per signature and celltype
 #' @return heatmap
-plot_signature_stability_heatmap<-function(signature_names){
-  stability_df_list<-list()
-  for (sig in signature_names){
-        df<-compute_signature_stability(baseline_signatures[[sig]])
-        df$signature<-sig
-        stability_df_list[[sig]]<-df
-  }
-  stability_df<-do.call(rbind,stability_df_list)
-  stability_df<-stability_df[stability_df$subset=="foreground",]
-  sig_subj_vars_all_fg<-as.data.frame(stability_df %>% group_by(celltype,signature) %>% summarize(median(subject_variance)))
-  colnames(sig_subj_vars_all_fg)<-c("celltype","signature","subject_variance")
+plot_signature_stability_heatmap<-function(multisignature_stability_df){
   celltypes<-c("bulk","B_Mem","B_Naive","CD4_Mem","CD4_Naive","CD8_Mem","CD8_Naive","ILC","MAIT","gdT_Vd1","gdT_Vd2","Mono_Classical","Mono_NonClassical","NK_CD16hi","NK_CD56hi","cDC","pDC")
-  sig_subj_vars_all_fg$celltype<-factor(sig_subj_vars_all_fg$celltype, levels=celltypes)
-  ggplot(data=sig_subj_vars_all_fg[sig_subj_vars_all_fg$celltype!="ILC",],aes(x=signature,y=celltype,fill=subject_variance))+geom_tile()  + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) + scale_fill_viridis_c(option = "magma")
+  multisignature_stability_df$celltype<-factor(multisignature_stability_df$celltype, levels=celltypes)
+  ggplot(data=multisignature_stability_df[multisignature_stability_df$celltype!="ILC",],aes(x=signature,y=celltype,fill=subject_variance))+geom_tile()  + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) + scale_fill_viridis_c(option = "magma")
 }
 
 
@@ -204,4 +226,35 @@ plot_signature_agetrend<-function(signature_rateOfchange, cell_subsets){
 plot_signature_gene_agetrend<-function(signature_rateOfchange, cell_subsets){
   sig<-unique(signature_rateOfchange$signature)
   ggplot(data=signature_rateOfchange[signature_rateOfchange$celltype %in% cell_subsets,], aes(x=window_number,y=mean_vexp, group=gene, colour=gene))+ geom_smooth(method = "loess", se = F) + theme_bw()+ggtitle(paste0(sig," - ",paste(cell_subsets,collapse = " / ")))
+}
+
+
+#' Plot Stacked Bars of Signature Variance Rate of Change Compostion per Celltype
+#'
+#' This function takes a data frame of the slope of subject variance per gene of
+#' a gene signature and celltype as input and categorizes each gene into increasing
+#' (positive slope, p<=0.05) or decreasing (negative slope, p<=0.05) or stable
+#' (p>0.05). It then visualizes the fractions of these categories as radarplot per
+#' celltype.
+#'
+#' @param signature_stability_slope data frame of subject variance slope of rate of change
+#' @return barplot
+plot_signature_stability_bars<-function(signature_stability_slope){
+  incr_decr_list<-list()
+  for (ct in unique(signature_stability_slope$celltype)){
+    sig<-unique(signature_stability_slope$signature)
+    decr<-sum(signature_stability_slope$window_number[((signature_stability_slope$celltype==ct)&(signature_stability_slope$p<=0.05))]<=0, na.rm = T)
+    incr<-sum(signature_stability_slope$window_number[((signature_stability_slope$celltype==ct)&(signature_stability_slope$p<=0.05))]>0, na.rm = T)
+    stab<-sum(abs(signature_stability_slope$p[(signature_stability_slope$celltype==ct)])>0.05, na.rm = T)
+    incr_decr_list[[ct]]<-c(decr,incr,stab)
+  }
+  df_incr_decr<-do.call(rbind,incr_decr_list)
+  colnames(df_incr_decr)<-c("decreasing","increasing","stable")
+  df_incr_decr<-as.data.frame(t(df_incr_decr))
+  for (name in names(df_incr_decr)){df_incr_decr[[name]]<-df_incr_decr[[name]]/colSums(df_incr_decr)[[name]]}
+  data <- df_incr_decr
+  data$slope<-rownames(data)
+  data<-melt(data, id.vars = "slope")
+  colnames(data)<-c("slope","celltype","value")
+  ggplot(data=data,aes(x=value,y=celltype,fill=slope))+geom_bar(stat='identity')+xlab("% variance explained")+ggtitle(sig)
 }
